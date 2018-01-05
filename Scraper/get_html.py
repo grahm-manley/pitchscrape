d4676217b683f78406d4d59e6aae09c7f3aa398e
grahm-manley <grahm.manley@gmail.com>
2018-01-05 16:47:19 -0600

    restructuring 2

--- Scraper/get_html.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-url = 'https://pitchfork.com/reviews/albums/?page=1'
-headers = {'User-Agent':'Mozilla/5.0'}
-response = requests.get(url, headers=headers)
-page = response.content
-print(page)
