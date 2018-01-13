1b22fd4aff1f34f68fc7d21be45283e48250a3f1
grahm-manley <grahm.manley@gmail.com>
2018-01-13 16:28:05 -0600

    Missed run.py in last commit

--- Scraper/run.py
+++ Scraper/run.py
@@ -1,6 +1,25 @@
 from scraper import Scraper
 from db_connection import DbConnection
+import time
+from datetime import datetime
 
+fails = []
 with DbConnection() as db:
-	for review in Scraper(db.get_last_update_time()).get_unsaved_reviews():
-		db.save_review()	
+	scrape = Scraper(db.get_last_update_time())
+	saved_reviews = 0
+	for review in scrape.get_unsaved_reviews():
+		print('Scraped: ', review.album_title, ' by ', review.artists)
+		try:
+			if(db.save_review(review)):
+				saved_reviews = saved_reviews + 1
+		except Exception as e:
+			print('Failed to save : ', review.album_title)
+			print('--------- Exception ----------------')
+			print(e)
+			print('------------------------------------')
+			fails.append((review.album_title, review.artists))
+			
+		time.sleep(1)	
+
+print("SCRAPE COMPLETED @ {}: Saved {} reviews with {} errors".format(
+	datetime.now(), saved_reviews, len(fails))) 
